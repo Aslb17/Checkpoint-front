4bf098a85bd127db00655a5c3b34c07ec1c7f502
Aslb <anne-sophie.leber@socgen.com>
2023-05-22 15:02:29 +0200

    add continents component

---
+++ frontend/src/Components/Continents.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const Continents: React.FC = () => {
+  const [continents, setContinents] = useState<string[]>([]);
+
+  useEffect(() => {
+    const client = new ApolloClient({
+      uri: "https://countries.nausicaa.wilders.dev/graphql",
+      cache: new InMemoryCache(),
+    });
+
+    client
+      .query({
+        query: gql`
+          query {
+            continents {
+              name
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        const continentNames = result.data.continents.map(
+          (continent: { name: string }) => continent.name
+        );
+        setContinents(continentNames);
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite :", error);
+      });
+  }, []);
+
+  return (
+    <div className="container text-center">
+                <h2 className="text-align mt-5">Continents</h2>
+      <div className="row row-cols-4 mt-5">
+
+          {continents.map((continent, index) => (
+            <div key={index} className="col border">{continent}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Continents;
